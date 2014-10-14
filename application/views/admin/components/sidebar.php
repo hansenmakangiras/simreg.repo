5ba243fa280c25a661979af02b30f059d253c25d
Hansen Makangiras <hansenmakangiras@gmail.com>
2014-10-14 21:08:57 +0800

    Added changes

---
+++ application/views/admin/components/sidebar.php
@@ -0,0 +1,132 @@
+        <div id="page-sidebar" class="scrollable-content">
+            <div id="sidebar-menu">
+                <ul>
+                    <li>
+                        <a href="<?php base_url('admin/backend');?>" title="Dashboard">
+                            <i class="glyph-icon icon-dashboard"></i>
+                            Dashboard
+                        </a>
+                    </li>
+                    <li>
+                        <a href="javascript:;" title="Components">
+                            <i class="glyph-icon icon-code"></i>
+                            Akademik
+                        </a>
+                        <ul>
+                            <li>
+                                <a href="<?php site_url('admin/berita')?>" title="Berita">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Data Instruktur
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="Informasi">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Data Taruna
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="Pendaftaran">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Data Pendaftaran
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="Alumni">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Data Mata Pelajaran
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="Pengurus">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Penetapan Kelas
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="Pesan">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Jadwal Mengajar
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php echo site_url('admin/user');?>" title="Managemen User">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Monitoring Ujian
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:;" title="Components">
+                            <i class="glyph-icon icon-code"></i>
+                            Kepegawaian
+                        </a>
+                        <ul>
+                            <li>
+                                <a href="<?php site_url('admin/berita')?>" title="Berita">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Data Pegawai
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="Informasi">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Data Golongan
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="Pendaftaran">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Data Gaji dan Honor
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="Alumni">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Data Pangkat
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="Pengurus">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Monitoring Pegawai
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="Pesan">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Monitoring Gaji
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php echo site_url('admin/user');?>" title="Managemen User">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Monitoring Status
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:;" title="Components">
+                            <i class="glyph-icon icon-code"></i>
+                            Setting
+                        </a>
+                        <ul>
+                             <li>
+                                <a href="<?php echo site_url('admin/user');?>" title="Managemen User">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Managemen User
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="Informasi">
+                                    <i class="glyph-icon icon-chevron-right"></i>
+                                    Maintenance
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div><!-- #page-sidebar -->
---
+++ application/views/admin/components/top-link.php
@@ -0,0 +1,49 @@
+            <div class="dropdown dash-menu">
+                <a href="" data-toggle="dropdown" data-placement="left" class="medium btn primary-bg float-right popover-button-header hidden-mobile tooltip-button" title="Example menu">
+                    <i class="glyph-icon icon-th"></i>
+                </a>
+                <div class="dropdown-menu float-right">
+                    <div class="small-box">
+                        
+                        <div class="bg-gray text-transform-upr font-size-12 font-bold font-gray-dark pad10A">Link Menu</div>
+                        <div class="pad10A dashboard-buttons clearfix">
+                            <a href="<?php echo base_url();?>" class="btn vertical-button remove-border bg-blue" title="">
+                                <span class="glyph-icon icon-separator-vertical pad0A medium">
+                                    <i class="glyph-icon icon-dashboard opacity-80 font-size-20"></i>
+                                </span>
+                                <span class="button-content">Dashboard</span>
+                            </a>
+                            <a href="javascript:;" class="btn vertical-button remove-border bg-red" title="">
+                                <span class="glyph-icon icon-separator-vertical pad0A medium">
+                                    <i class="glyph-icon icon-tags opacity-80 font-size-20"></i>
+                                </span>
+                                <span class="button-content">Berita</span>
+                            </a>
+                            <a href="javascript:;" class="btn vertical-button remove-border bg-purple" title="">
+                                <span class="glyph-icon icon-separator-vertical pad0A medium">
+                                    <i class="glyph-icon icon-reorder opacity-80 font-size-20"></i>
+                                </span>
+                                <span class="button-content">Informasi</span>
+                            </a>
+                            <a href="javascript:;" class="btn vertical-button remove-border bg-azure" title="">
+                                <span class="glyph-icon icon-separator-vertical pad0A medium">
+                                    <i class="glyph-icon icon-bar-chart opacity-80 font-size-20"></i>
+                                </span>
+                                <span class="button-content">Pesan</span>
+                            </a>
+                            <a href="javascript:;" class="btn vertical-button remove-border bg-yellow" title="">
+                                <span class="glyph-icon icon-separator-vertical pad0A medium">
+                                    <i class="glyph-icon icon-laptop opacity-80 font-size-20"></i>
+                                </span>
+                                <span class="button-content">Setting</span>
+                            </a>
+                            <a href="javascript:;" class="btn vertical-button remove-border bg-orange" title="">
+                                <span class="glyph-icon icon-separator-vertical pad0A medium">
+                                    <i class="glyph-icon icon-code opacity-80 font-size-20"></i>
+                                </span>
+                                <span class="button-content">Upload</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
