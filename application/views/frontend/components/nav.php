e25aa4e7fae73518e9a3b3acef303e3633af0f85
Hansen Makangiras <hansenmakangiras@gmail.com>
2014-10-14 12:20:48 +0800

    added changes layout

---
+++ application/views/frontend/components/nav.php
@@ -0,0 +1,25 @@
+<div class="nav-collapse collapse">
+    <ul class="nav">
+        <li class="">
+            <a href="<?php echo base_url() ?>">Beranda</a>
+        </li>
+        <li>
+            <a href="<?php echo site_url('profil'); ?>">Profil PMK</a>
+        </li>
+        <li class="">
+            <a href="<?php echo site_url('divisi'); ?>">Divisi</a>
+        </li>
+        <li class="">
+            <a href="<?php echo site_url('blog'); ?>">Blog PMK</a>
+        </li>
+        <li class="">
+            <a href="<?php echo site_url('donasi'); ?>">Donasi</a>
+        </li>
+        <li class="">
+            <a href="<?php echo site_url('kegiatan'); ?>">Kegiatan</a>
+        </li>
+        <li class="">
+            <a href="<?php echo site_url('login'); ?>">Login</a>
+        </li>
+    </ul>
+</div><!-- End Navigasi Menu -->
