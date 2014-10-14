b77575d864f78b30953f049b3f3cae16874ff11b
Hansen Makangiras <hansenmakangiras@gmail.com>
2014-10-14 18:23:16 +0800

    Added New Pages

---
+++ application/views/frontend/pages/profil-pmk.php
@@ -0,0 +1,56 @@
+
+    <div class="container">
+        <div class="row-fluid">
+            <div class="span12">
+                <h2>Profil PMK</h2>
+                <p>
+                    PMK LP3i lahir dari adanya kesadaran dan kerinduan dari mahasiswa(i) kristen LP3i yang merasa perlu adanya sebuah persekutuan kristen di tengah-tengah kampus Lp3i.
+                    Mengingat posisi kita yang notabene sebagai mahasiswa(i) yang minoritas,oleh karena itu setiap anggota dituntut untuk bisa mencerminkan sikap  kristiani yaitu kasih.
+                </p>
+                <p>
+                    PMK LP3i Makassar terbentuk pada tanggal 04 December 2002, dengan kerinduan agar:
+                </p>                
+                    <ul>
+                        <li>Setiap mahasiwa(i) kristen Lp3i memiliki wadah yang dapat menyatukan setiap aspirasi anggotanya.</li>
+                        <li>Setiap mahasiwa(i) kristen Lp3i dapat saling berinteraksi , dilayani dan melayani serta saling bertukar informasi. (study,relasi,tempat kos,bisnis,kerohanian dll.)</li>
+                        <li>PMK LP3i sebagai wadah pengembangan diri untuk menciptakan kader-kader rohani dan pemimpin masa depan yang profesional.</li>
+                    </ul>
+                <p>
+                    Setiap anggota tidak di batasi oleh aturan-aturan atau ketentuan khusus yang harus di jalankan ,namun setiap anggota terikat oleh beban rohani yang mana harus menjadi garam dan terang di tengah dunia ini .
+                </p>                
+                    <h3>ARTI LOGO:</h3>
+                    <div class="span6">
+                        <p><img src="<?php echo base_url() ;?>" class="thumbnail"></p>
+                    </div>
+                    <div class="span6">
+                        <div class="table">
+                        <table class="span12">                            
+                            <tbody>
+                                <tr>
+                                    <td>Merpati</td>
+                                    <td>:</td>                                                                        
+                                    <td>Perwujudan Roh Kudus</td>                                                                        
+                                </tr>
+                                <tr>
+                                    <td>Salib</td>
+                                    <td>:</td>                                                                        
+                                    <td>Pengorbanan Tuhan Yesus</td>                                                                        
+                                </tr>
+                                <tr>
+                                    <td>Bola Dunia</td>
+                                    <td>:</td>                                                                        
+                                    <td>Lingkungan Dimana Kita Berada</td>                                                                        
+                                </tr>
+                                <tr>
+                                    <td>Alkitab</td>
+                                    <td>:</td>                                                                        
+                                    <td>Firman Tuhan</td>                                                                        
+                                </tr>                                
+                            </tbody>                            
+                        </table>                        
+                    </div>  
+                    </div>
+                                  
+            </div><!--/span-->            
+        </div><!--row-fluid-->
+    </div><!-- Container -->
