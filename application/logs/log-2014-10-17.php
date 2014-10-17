115778969137fbd3b1ce7ac90b2a15d89708ef5c
Hansen Makangiras <hansenmakangiras@gmail.com>
2014-10-17 14:16:57 +0800

    Added Changes

--- application/logs/log-2014-10-17.php
+++ application/logs/log-2014-10-17.php
@@ -170,3 +170,25 @@ ERROR - 2014-10-17 13:08:28 --> Severity: Warning  --> mysql_query() expects par
 ERROR - 2014-10-17 13:08:29 --> 404 Page Not Found --> css
 ERROR - 2014-10-17 13:08:29 --> 404 Page Not Found --> assets
 ERROR - 2014-10-17 13:08:29 --> 404 Page Not Found --> assets
+ERROR - 2014-10-17 13:15:18 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:15:18 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:15:18 --> Unable to connect to the database
+ERROR - 2014-10-17 13:15:18 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:15:19 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:15:19 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:15:19 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:15:19 --> Unable to connect to the database
+ERROR - 2014-10-17 13:15:19 --> Severity: Warning  --> mysql_query() expects parameter 2 to be resource, boolean given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 179
+ERROR - 2014-10-17 13:15:19 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2014-10-17 13:15:19 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2014-10-17 13:15:19 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:15:19 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:15:19 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:15:19 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:15:19 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:15:19 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:15:19 --> Unable to connect to the database
+ERROR - 2014-10-17 13:15:19 --> Severity: Warning  --> mysql_query() expects parameter 2 to be resource, boolean given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 179
+ERROR - 2014-10-17 13:15:19 --> 404 Page Not Found --> css
+ERROR - 2014-10-17 13:15:20 --> 404 Page Not Found --> assets
+ERROR - 2014-10-17 13:15:20 --> 404 Page Not Found --> assets
