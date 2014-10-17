a9a774b36588c111a2b3d30ecd0bcf9a00d28b5f
Hansen Makangiras <hansenmakangiras@gmail.com>
2014-10-17 14:11:38 +0800

    Added Changes

--- application/logs/log-2014-10-17.php
+++ application/logs/log-2014-10-17.php
@@ -93,3 +93,80 @@ ERROR - 2014-10-17 02:38:33 --> Severity: Warning  --> mysql_query() expects par
 ERROR - 2014-10-17 02:38:35 --> 404 Page Not Found --> css
 ERROR - 2014-10-17 02:38:35 --> 404 Page Not Found --> assets
 ERROR - 2014-10-17 02:38:35 --> 404 Page Not Found --> assets
+ERROR - 2014-10-17 12:54:41 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 12:54:41 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 12:54:41 --> Unable to connect to the database
+ERROR - 2014-10-17 12:54:41 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 12:54:41 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 12:54:41 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 12:54:41 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 12:54:41 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 12:54:41 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 12:54:41 --> Unable to connect to the database
+ERROR - 2014-10-17 12:54:41 --> Severity: Warning  --> mysql_query() expects parameter 2 to be resource, boolean given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 179
+ERROR - 2014-10-17 12:54:42 --> 404 Page Not Found --> css
+ERROR - 2014-10-17 12:55:07 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 12:55:07 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 12:55:07 --> Unable to connect to the database
+ERROR - 2014-10-17 12:55:07 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 12:55:08 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 12:55:08 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 12:55:08 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 12:55:08 --> Unable to connect to the database
+ERROR - 2014-10-17 12:55:08 --> Severity: Warning  --> mysql_query() expects parameter 2 to be resource, boolean given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 179
+ERROR - 2014-10-17 12:55:08 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2014-10-17 12:55:08 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2014-10-17 12:55:08 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 12:55:08 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 12:55:08 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 12:55:08 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 12:55:08 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 12:55:08 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 12:55:08 --> Unable to connect to the database
+ERROR - 2014-10-17 12:55:08 --> Severity: Warning  --> mysql_query() expects parameter 2 to be resource, boolean given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 179
+ERROR - 2014-10-17 12:55:09 --> 404 Page Not Found --> css
+ERROR - 2014-10-17 13:01:53 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:01:53 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:01:53 --> Unable to connect to the database
+ERROR - 2014-10-17 13:01:54 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:01:54 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:01:54 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:01:54 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:01:54 --> Unable to connect to the database
+ERROR - 2014-10-17 13:01:54 --> Severity: Warning  --> mysql_query() expects parameter 2 to be resource, boolean given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 179
+ERROR - 2014-10-17 13:01:54 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2014-10-17 13:01:54 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2014-10-17 13:01:54 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:01:54 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:01:54 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:01:54 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:01:54 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:01:54 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:01:54 --> Unable to connect to the database
+ERROR - 2014-10-17 13:01:54 --> Severity: Warning  --> mysql_query() expects parameter 2 to be resource, boolean given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 179
+ERROR - 2014-10-17 13:01:55 --> 404 Page Not Found --> css
+ERROR - 2014-10-17 13:01:55 --> 404 Page Not Found --> assets
+ERROR - 2014-10-17 13:01:55 --> 404 Page Not Found --> assets
+ERROR - 2014-10-17 13:01:58 --> 404 Page Not Found --> assets
+ERROR - 2014-10-17 13:08:28 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:08:28 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:08:28 --> Unable to connect to the database
+ERROR - 2014-10-17 13:08:28 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:08:28 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:08:28 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:08:28 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:08:28 --> Unable to connect to the database
+ERROR - 2014-10-17 13:08:28 --> Severity: Warning  --> mysql_query() expects parameter 2 to be resource, boolean given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 179
+ERROR - 2014-10-17 13:08:28 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2014-10-17 13:08:28 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2014-10-17 13:08:28 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:08:28 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:08:28 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:08:28 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2014-10-17 13:08:28 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:08:28 --> Severity: Warning  --> mysql_connect(): Access denied for user ''@'localhost' (using password: NO) E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2014-10-17 13:08:28 --> Unable to connect to the database
+ERROR - 2014-10-17 13:08:28 --> Severity: Warning  --> mysql_query() expects parameter 2 to be resource, boolean given E:\Webserver\Web\simreg.repo\system\database\drivers\mysql\mysql_driver.php 179
+ERROR - 2014-10-17 13:08:29 --> 404 Page Not Found --> css
+ERROR - 2014-10-17 13:08:29 --> 404 Page Not Found --> assets
+ERROR - 2014-10-17 13:08:29 --> 404 Page Not Found --> assets
